f47697a7c0213afcf49e43d16456b5b7884bc00b
abdulnelfrank <nelfrankaj@gmail.com>
2022-05-22 05:05:29 +0300

    user app initial commit

--- users/models.py
+++ users/models.py
@@ -1,3 +1,49 @@
+import uuid
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+
 
 # Create your models here.
+class BaseModel(models.Model):
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+    date_deleted = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        abstract = True
+
+
+class ProfileMixin(BaseModel):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    first_name = models.CharField(max_length=255)
+    middle_name = models.CharField(max_length=255, blank=True, null=True)
+    last_name = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=255, blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
+    postal_address = models.CharField(max_length=255, blank=True, null=True)
+    physical_address = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        middle_name = self.middle_name
+        return "%s %s %s" % (self.first_name, middle_name if middle_name else "", self.last_name)
+
+    class Meta:
+        abstract = True
+
+
+class User(BaseModel, AbstractBaseUser, PermissionsMixin):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    username = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.username
+
+
+class Staff(ProfileMixin):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="staff_user")
+    employee_num = models.CharField(max_length=255, blank=True, null=True)
+
+
+class PublicUser(ProfileMixin):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="public_user")
+
