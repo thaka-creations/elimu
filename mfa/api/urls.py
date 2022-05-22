408691b3ffc3cc868196398e593e55d0afd54597
abdulnelfrank <nelfrankaj@gmail.com>
2022-05-22 11:13:25 +0300

    mfa updates

--- mfa/api/urls.py
+++ mfa/api/urls.py
@@ -1,2 +1,7 @@
 from rest_framework.routers import DefaultRouter
 from mfa.api import views
+
+router = DefaultRouter(trailing_slash=False)
+router.register('otp', views.OtpViewSet, basename='otp')
+
+urlpatterns = router.urls
--- mfa/api/views.py
+++ mfa/api/views.py
@@ -1,9 +1,14 @@
+from datetime import timedelta
 from django.db import transaction
+from django.utils import timezone
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from mfa.api import serializers as mfa_serializers
+from mfa.utils import two_factor
 from mfa import models as mfa_models
+
+klass = two_factor.MultiFactorAuthentication()
 
 
 class OtpViewSet(viewsets.ViewSet):
@@ -23,3 +28,41 @@ class OtpViewSet(viewsets.ViewSet):
             return Response({"details": payload_serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
         validated_data = payload_serializer.validated_data
+        send_to = validated_data['send_to']
+        expiry_time = int(validated_data['expiry_time'])
+        generation_time = timezone.now()
+        expiry_date = generation_time + timedelta(seconds=expiry_time)
+        otp_code = klass.generate_otp_code()
+
+        with transaction.atomic():
+            otp_instance = mfa_models.OtpCode.objects.create(
+                code=otp_code,
+                send_to=send_to,
+                expiry_date=expiry_date
+            )
+            serializer = mfa_serializers.OtpSerializer(otp_instance, many=False)
+            return Response({"details": serializer.data}, status=status.HTTP_200_OK)
+
+    @action(
+        methods=['POST'],
+        detail=False,
+        url_name='verify',
+        url_path='verify'
+    )
+    def verify_otp_code(self, request):
+        payload_serializer = mfa_serializers.VerifyOtpSerializer(
+            data=request.data, many=False
+        )
+
+        if not payload_serializer.is_valid():
+            return Response({"details": payload_serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+        validated_data = payload_serializer.validated_data
+        otp = validated_data['otp']
+        send_to = validated_data['send_to']
+        valid, message = klass.verify_otp_code(otp, send_to)
+
+        if not valid:
+            return Response({"details": message}, status=status.HTTP_400_BAD_REQUEST)
+
+        return Response({"details": message}, status=status.HTTP_200_OK)
