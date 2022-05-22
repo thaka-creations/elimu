4146ede392a158453e61c8d6935cf2da72cd2101
abdulnelfrank <nelfrankaj@gmail.com>
2022-05-22 11:42:28 +0300

    elimu updates

--- users/utils/services.py
+++ users/utils/services.py
@@ -1,7 +1,20 @@
 import requests
 from django.conf import settings
 
-CALLBACK_URL = settings.SERVICES_URLS()
+CALLBACK_URL = settings.SERVICES_URLS('callback_url')
+
 
 def generate_otp_code(payload):
-    url =
+    url = CALLBACK_URL + 'mfa/otp/generate'
+    resp = requests.post(url, data=payload)
+    if resp.status_code == 200:
+        return resp.json()
+    return False
+
+
+def verify_otp_code(payload):
+    url = CALLBACK_URL + 'mfa/otp/verify'
+    resp = requests.post(url, data=payload)
+    if resp.status_code == 200:
+        return True, resp.json()
+    return False, resp.json()
