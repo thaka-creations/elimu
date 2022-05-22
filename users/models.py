3cf24e1bbd32e999233b3980c3be8b2f4410c4ae
abdulnelfrank <nelfrankaj@gmail.com>
2022-05-22 05:12:27 +0300

    user updates

--- users/models.py
+++ users/models.py
@@ -2,8 +2,15 @@ import uuid
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
-
 # Create your models here.
+ACCEPTED_STATUS = [
+    ("ACTIVE", "ACTIVE"),
+    ("SUSPENDED", "SUSPENDED"),
+    ("DEACTIVATED", "DEACTIVATED"),
+    ("REGISTRATION", "REGISTRATION")
+]
+
+
 class BaseModel(models.Model):
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
@@ -22,6 +29,7 @@ class ProfileMixin(BaseModel):
     email = models.EmailField(blank=True, null=True)
     postal_address = models.CharField(max_length=255, blank=True, null=True)
     physical_address = models.CharField(max_length=255, blank=True, null=True)
+    profile_status = models.CharField(max_length=255, choices=ACCEPTED_STATUS, default="REGISTRATION")
 
     def __str__(self):
         middle_name = self.middle_name
@@ -34,9 +42,18 @@ class ProfileMixin(BaseModel):
 class User(BaseModel, AbstractBaseUser, PermissionsMixin):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     username = models.CharField(max_length=255, unique=True)
+    account_status = models.CharField(max_length=255, choices=ACCEPTED_STATUS, default="REGISTRATION")
+
+    USERNAME_FIELD = 'username'
 
     def __str__(self):
         return self.username
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
 
 
 class Staff(ProfileMixin):
@@ -46,4 +63,3 @@ class Staff(ProfileMixin):
 
 class PublicUser(ProfileMixin):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="public_user")
-
