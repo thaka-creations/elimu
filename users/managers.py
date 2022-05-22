3cfea30b2f916dcd7559ab4bb8ddcbe4a772bbb6
abdulnelfrank <nelfrankaj@gmail.com>
2022-05-22 05:31:24 +0300

    user updates

--- users/managers.py
+++ users/managers.py
@@ -2,13 +2,12 @@ from django.contrib.auth.base_user import BaseUserManager
 
 
 class UserManager(BaseUserManager):
-    def create_user(self, username, is_staff, password=None):
+    def create_user(self, username, password=None):
         if not username:
             pass
 
         user = self.model(
-            username=username,
-            is_staff=is_staff
+            username=username
         )
         user.set_password(password)
         user.save(using=self.db)
@@ -17,11 +16,11 @@ class UserManager(BaseUserManager):
     def create_superuser(self, username, password=None):
         user = self.create_user(
             username=username,
-            is_staff=True,
             password=password
         )
 
         user.is_admin = True
+        user.is_staff = True
         user.is_active = True
         user.account_status = "ACTIVE"
         user.save(using=self._db)
