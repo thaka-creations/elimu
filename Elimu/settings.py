a37a3a5cb3f73c00c5a24c2a2eaa3db56d7078e9
abdulnelfrank <nelfrankaj@gmail.com>
2022-05-22 05:06:16 +0300

    elimu updates

--- Elimu/settings.py
+++ Elimu/settings.py
@@ -119,6 +119,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+AUTH_USER_MODEL = 'users.User'
+
 STATIC_URL = 'static/'
 
 # Default primary key field type
