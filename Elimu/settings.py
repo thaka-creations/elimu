352cbf495f3b5ec4c9f1f782a1147259066e63d4
abdulnelfrank <nelfrankaj@gmail.com>
2022-05-30 20:31:13 +0300

    requirements updates

--- Elimu/settings.py
+++ Elimu/settings.py
@@ -11,8 +11,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
 from dotenv import load_dotenv
-from
+from pathlib import Path
 
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -22,6 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
+TOKEN_SECRET_KEY = os.environ.get('TOKEN_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG")
