dfa5ef12f1ae5cbb076250e1377aff1f7684c1a9
abdulnelfrank <nelfrankaj@gmail.com>
2022-06-02 22:31:01 +0300

    acl updates

--- school/urls.py
+++ school/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('form/<pk>', views.FormView.as_view())
+    path('form/<pk>', views.FormView.as_view()),
+    path('form/subject/<pk>', views.SubjectView.as_view()),
+    path('form/subject/unit/<pk>', views.UnitView.as_view()),
 ]
--- school/views.py
+++ school/views.py
@@ -23,3 +23,31 @@ class FormView(View):
         context = {"subjects": qs, "instance": inst, "num": num}
         return render(request, self.template_name, context=context)
 
+class SubjectView(View):
+    template_name = "school/subjects/index.html"
+
+    def get(self, request, pk):
+        try:
+            instance = school_models.SubjectModel.objects.get(pk=pk)
+        except school_models.SubjectModel.DoesNotExist:
+            return redirect("/")
+
+        units = school_models.UnitModel.objects.filter(subject=instance)
+        context = {"units": units, "subject": instance}
+        return render(request, self.template_name, context=context)     
+
+class UnitView(View): 
+    template_name = "school/subjects/units/index.html"
+
+    def get(self, request, pk):
+        try:
+            instance = school_models.UnitModel.objects.get(pk=pk)
+        except school_models.UnitModel.DoesNotExist:
+            return redirect("/")
+
+        qs = school_models.VideoModel.objects.filter(unit=instance)
+        context = {"videos": qs}
+        return render(request, self.template_name, context=context)
+
+
+
