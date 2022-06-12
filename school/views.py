ee625fa368087ae62c6aa27d22ad9773935d3c4d
abdulnelfrank <nelfrankaj@gmail.com>
2022-06-12 12:29:47 +0300

    admin updates

--- school/views.py
+++ school/views.py
@@ -1,7 +1,12 @@
+import requests
+import uuid
 from word2number import w2n
 from django.shortcuts import render, redirect
+from django.conf import settings
 from django.views import View
 from school import models as school_models
+
+CALLBACK_URL = settings.SERVICES_URLS['callback_url']
 
 
 class FormView(View):
@@ -19,7 +24,7 @@ class FormView(View):
         except Exception as e:
             return redirect("/")
 
-        qs = school_models.SubjectModel.objects.\
+        qs = school_models.SubjectModel.objects. \
             filter(form_units__videos__isnull=False, form_units__form=inst).distinct()
         context = {"subjects": qs, "instance": inst, "num": num}
         return render(request, self.template_name, context=context)
@@ -47,11 +52,39 @@ class UnitView(View):
     template_name = "school/subjects/units/index.html"
 
     def get(self, request, pk):
+        videoid = request.GET.get("videoid", False)
         try:
             instance = school_models.UnitModel.objects.get(pk=pk)
         except school_models.UnitModel.DoesNotExist:
             return redirect("/")
 
         qs = school_models.VideoModel.objects.filter(unit=instance)
-        context = {"videos": qs, 'unit': instance}
+
+        if not qs.exists():
+            return redirect("/")
+
+        if videoid:
+            try:
+                uuid.UUID(videoid)
+            except ValueError:
+                return redirect("/")
+
+            try:
+                video_id = school_models.VideoModel.objects.get(id=videoid).videoid
+            except school_models.VideoModel.DoesNotExist:
+                return redirect("/")
+        else:
+            video_id = qs.first().videoid
+        url = CALLBACK_URL + 'video/get-video-otp'
+        headers = {"Authorization": "Apisecret " + settings.VDOCIPHER_SECRET}
+
+        resp = requests.get(url, params={"video_id": video_id}, headers=headers)
+        res = resp.json()
+        if not res:
+            return redirect("/")
+        else:
+            otp = res['otp']
+            playback = res['playbackInfo']
+
+        context = {"videos": qs, 'unit': instance, 'otp': otp, 'playback': playback, 'video_id': video_id}
         return render(request, self.template_name, context=context)
