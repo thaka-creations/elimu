569fe12dbec0d93dec9d4e4256005512aa0c3cbf
abdulnelfrank <nelfrankaj@gmail.com>
2022-06-27 22:43:24 +0300

    subject payment

--- Elimu/settings.py
+++ Elimu/settings.py
@@ -173,11 +173,11 @@ SERVICES_URLS = {
 LOGIN_URL = "/login"
 LOGOUT_REDIRECT_URL = "/login"
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
 
-# STATICFILES_DIRS = [BASE_DIR / "static", '/var/www/static/']
+STATICFILES_DIRS = [BASE_DIR / "static", '/var/www/static/']
 STATIC_ROOT = "/var/www/static/"
 
 MEDIA_URL = '/media/'
