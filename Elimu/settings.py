c82959920b810dea6e37a7404807709f989ce75c
abdulnelfrank <nelfrankaj@gmail.com>
2022-06-29 18:54:24 +0300

    settings updates

--- Elimu/settings.py
+++ Elimu/settings.py
@@ -177,7 +177,7 @@ LOGOUT_REDIRECT_URL = "/login"
 #     os.path.join(BASE_DIR, 'static'),
 # ]
 
-STATICFILES_DIRS = [BASE_DIR / "static", '/var/www/static/']
+# STATICFILES_DIRS = ['/var/www/static/']
 STATIC_ROOT = "/var/www/static/"
 
 MEDIA_URL = '/media/'
