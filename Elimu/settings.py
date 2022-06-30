dfe88dc170b3afa19254a15730b5821c78abec57
abdulnelfrank <nelfrankaj@gmail.com>
2022-06-30 23:40:17 +0300

    settings updates

--- Elimu/settings.py
+++ Elimu/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 TOKEN_SECRET_KEY = os.environ.get('TOKEN_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
