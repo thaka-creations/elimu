4b32ae07fa884b7a7659e568f589be612c3d54b8
abdulnelfrank <nelfrankaj@gmail.com>
2022-07-10 22:18:48 +0300

    updates

--- Elimu/settings.py
+++ Elimu/settings.py
@@ -192,5 +192,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'thakacreations@gmail.com'
-EMAIL_HOST_PASSWORD = 'zbvnfbasbhktjlst'
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
--- users/forms.py
+++ users/forms.py
@@ -8,23 +8,28 @@ from users import validators, models as user_models
 
 class RegistrationForm(forms.Form):
     name = forms.CharField(label="Full Name", max_length=255, required=True, label_suffix="",
-                           widget=forms.TextInput(attrs={"class": "form-control shadow-none rounded-0"}))
+                           widget=forms.TextInput(attrs={"class": "form-control shadow-none rounded-0", "autocomplete": "off"}))
     email = forms.EmailField(label="Email Address", required=True, label_suffix="",
-                             widget=forms.EmailInput(attrs={"class": "form-control shadow-none rounded-0"}))
+                             widget=forms.EmailInput(attrs={"class": "form-control shadow-none rounded-0",
+                                                            "autocomplete": "off", "id": "in-email"}))
     school = forms.CharField(label="School", max_length=255, required=True, label_suffix="",
-                             widget=forms.TextInput(attrs={"class": "form-control shadow-none rounded-0"}))
+                             widget=forms.TextInput(attrs={"class": "form-control shadow-none rounded-0",
+                                                           "autocomplete": "off"}))
     county = forms.ModelChoiceField(queryset=County.objects.filter(status=True), label="County", label_suffix="",
                                     required=True,
-                                    widget=forms.Select(attrs={"class": "form-control shadow-none rounded-0"}))
+                                    widget=forms.Select(attrs={"class": "form-control shadow-none rounded-0",
+                                                               "autocomplete": "off"}))
     code = forms.ModelChoiceField(queryset=user_models.Agent.objects.all(), label="Registration Code",
                                   label_suffix="", required=True,
-                                  widget=forms.Select(attrs={"class": "form-control shadow-none rounded-0"}))
+                                  widget=forms.Select(attrs={"class": "form-control shadow-none rounded-0",
+                                                             "autocomplete": "off"}))
     password = forms.CharField(label="Password", max_length=100, required=True, label_suffix="", min_length=6,
-                               widget=forms.PasswordInput(attrs={"class": "form-control shadow-none rounded-0"}))
+                               widget=forms.PasswordInput(attrs={"class": "form-control shadow-none rounded-0",
+                                                                 "autocomplete": "off"}))
     confirm_password = forms.CharField(label="Confirm Password", max_length=100, required=True, label_suffix="",
                                        min_length=6,
                                        widget=forms.PasswordInput(
-                                           attrs={"class": "form-control shadow-none rounded-0"}))
+                                           attrs={"class": "form-control shadow-none rounded-0", "autocomplete": "off"}))
 
     def clean(self):
         cleaned_data = super(RegistrationForm, self).clean()
